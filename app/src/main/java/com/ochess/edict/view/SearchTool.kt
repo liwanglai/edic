5c5b1af3689708b2d23664c397e6ee051b6360cf
shang <kelitq@qq.com>
2024-12-26 11:21:37 +0800

    搜索的单词没有记录到历史记录中去

--- app/src/main/java/com/ochess/edict/view/SearchTool.kt
+++ app/src/main/java/com/ochess/edict/view/SearchTool.kt
@@ -114,9 +114,9 @@ fun SearchTool(wordViewModel :WordModelViewModel){
                         && wordViewModel.wordState.value.wordModel!=null
                         && beforState!!.wordModel!!.word != wordViewModel.wordState.value.wordModel!!.word
                     ) {
-                        BookConf.instance.next(wordViewModel.wordState.value.wordModel!!)
                         //插入历史记录
                         wordViewModel.insertHistory(wordViewModel.wordState.value.wordModel!!)
+                        BookConf.instance.next(wordViewModel.wordState.value.wordModel!!)
                     }
                 },
                 //完成按钮单击
